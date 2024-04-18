425136d96901e387b22a52da02879fc6b2f2f136
Andrew Chen <andrew831119work@gmail.com>
2024-04-18 17:52:48 +0800

    temp test

--- src/pages/Article.tsx
+++ src/pages/Article.tsx
@@ -1,31 +1,32 @@
+import React from "react";
 // import React, { useEffect, useState } from "react";
 
-// // firebase
+// firebase
 // import { db } from "../main";
 // import { doc, getDoc } from "firebase/firestore";
 // import { useParams } from "react-router-dom";
 
-// const Article: React.FC = () => {
-//   const [isLoading, setIsLoading] = useState<boolean>(false);
-//   const [article, setArticle] = useState<DocumentData[] | null>(null);
+const Article: React.FC = () => {
+  //   const [isLoading, setIsLoading] = useState<boolean>(false);
+  //   const [article, setArticle] = useState<DocumentData[] | null>(null);
 
-//   const { id } = useParams();
+  //   const { id } = useParams();
 
-//   const fetchArticleFromFirebase = async (): Promise<void> => {
-//     setIsLoading(true);
-//     try {
-//       // const docRef = doc(db, "articles", id);
-//       // const docSnap = await getDoc(docRef);
-//       // setArticle(docSnap.data());
-//     } catch (error) {
-//       console.log(error);
-//     }
-//     setIsLoading(false);
-//   };
+  //   const fetchArticleFromFirebase = async (): Promise<void> => {
+  //     setIsLoading(true);
+  //     try {
+  //       // const docRef = doc(db, "articles", id);
+  //       // const docSnap = await getDoc(docRef);
+  //       // setArticle(docSnap.data());
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //     setIsLoading(false);
+  //   };
 
-//   useEffect(() => {}, []);
+  //   useEffect(() => {}, []);
 
-//   return <div className="mx-auto w-[70%] max-w-5xl"></div>;
-// };
+  return <div className="mx-auto w-[70%] max-w-5xl"></div>;
+};
 
-// export default Article;
+export default Article;
