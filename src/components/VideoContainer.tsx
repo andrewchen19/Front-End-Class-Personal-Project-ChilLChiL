01e69c6d4e18b7167287c5c548916e8484e1ab70
Andrew Chen <andrew831119work@gmail.com>
2024-05-18 18:29:27 +0800

    fix: reduce size of video

--- src/components/VideoContainer.tsx
+++ src/components/VideoContainer.tsx
@@ -1,6 +1,5 @@
 import React from "react";
-import surfVideo from "../assets/videos/3125821-uhd_3840_2160_24fps.mp4";
-// import surfVideo2 from "../assets/videos/202405131712.mp4";
+import surfVideo from "../assets/videos/surf-video-reduce.mp4";
 
 const VideoContainer: React.FC = () => {
   return (
