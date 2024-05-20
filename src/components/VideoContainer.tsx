1a42ad39ddb8c53e4ada700e99d1e78d13c6a5f5
Andrew Chen <andrew831119work@gmail.com>
2024-05-20 21:00:10 +0800

    feat: add poster in VideoContainer

--- src/components/VideoContainer.tsx
+++ src/components/VideoContainer.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import surfVideo from "../assets/videos/surf-video-reduce.mp4";
+import posterImg from "../assets/images/poster.png";
 
 const VideoContainer: React.FC = () => {
   return (
@@ -8,6 +9,7 @@ const VideoContainer: React.FC = () => {
       autoPlay
       muted
       preload="auto"
+      poster={posterImg}
       className="h-full w-full object-cover object-center"
     >
       <source src={surfVideo} type="video/mp4" />
