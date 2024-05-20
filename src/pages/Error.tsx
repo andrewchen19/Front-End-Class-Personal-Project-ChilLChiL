d1875ad81f591524ad531a384b6584207504e251
Andrew Chen <andrew831119work@gmail.com>
2024-05-20 19:41:51 +0800

    feat: create not found page

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import notFound from "../assets/images/not-found.svg";
+import { NavLink, useRouteError, isRouteErrorResponse } from "react-router-dom";
+
+// shadcn
+import { Button } from "@/components/ui/button";
 
 const Error: React.FC = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+
+  if (isRouteErrorResponse(error)) {
+    if (error.status === 404) {
+      return (
+        <main className="flex min-h-screen items-center justify-center">
+          <section className="gay-y-8 grid items-center justify-center gap-x-16 lg:grid-cols-2">
+            {/* image */}
+            <div className="max-w-[500px]">
+              <img src={notFound} alt="not found" className="w-full" />
+            </div>
+            {/* info */}
+            <div className="text-center">
+              <h1 className="text-3xl font-bold capitalize lg:text-4xl">
+                page not found
+              </h1>
+              <p className="mb-4 mt-4 leading-7 tracking-wide">
+                Sorry, we couldn't find the page you're looking for
+              </p>
+              <NavLink to="/">
+                <Button size={"lg"} variant={"turquoise-hipster"}>
+                  Home
+                </Button>
+              </NavLink>
+            </div>
+          </section>
+        </main>
+      );
+    }
+  }
 };
 
 export default Error;
